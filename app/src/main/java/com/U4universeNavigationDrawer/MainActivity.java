c8b79d849739dbad1090ea33deb6957865979762
vinaytshetty <vinaytshetty@gmail.com>
2020-05-01 17:09:26 +0530

    Added ToolBar Transperencey,Issue is its showing white patch type.

--- app/src/main/java/com/U4universeNavigationDrawer/MainActivity.java
+++ app/src/main/java/com/U4universeNavigationDrawer/MainActivity.java
@@ -4,11 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mdrawerLayout;
+    Toolbar mtoolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nav_drawer_layout);
+        initViews();
+        usetoolbar();
+        addHumberDesingnAndSync();
+    }
+
+
+    private void initViews() {
+        mtoolbar = (Toolbar) findViewById(R.id.toolbar_mainActivity);
+        mdrawerLayout = findViewById(R.id.drawerlayout);
+    }
+
+    private void usetoolbar() {
+        setSupportActionBar(mtoolbar);
+    }
+
+    private void addHumberDesingnAndSync(){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                mdrawerLayout,
+                mtoolbar,
+                R.string.nav_open,
+                R.string.nav_close);  // Here close and open String used for Blind people using Navigation Drawer.
+        mdrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(mdrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mdrawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
+
